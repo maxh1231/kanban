d8d7026789c287471b1527d070c9b66dd378cdc2
maxh1231 <maxhumpherys@gmail.com>
2022-09-11 13:16:11 -0600

    new packages

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,12 @@
 // mongo setup
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kanban', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
