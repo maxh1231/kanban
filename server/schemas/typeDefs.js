83cdc959bb9ffb54c6d76ad2dc7a831457ec0de4
maxh1231 <maxhumpherys@gmail.com>
2022-09-11 13:23:01 -0600

    server and typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        createdAt: String!
+    }
+    type Query {
+        users: [User]
+        user(username: String!): User
+        me: User
+        loggedIn: String
+    }
+    type Mutation {
+        signup(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): User
+        logout: User
+    }
+`
+
+module.exports = typeDefs;
---
+++ server/utils/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    authMiddleware: function ({ req }) {
+        return req
+    }
+};
