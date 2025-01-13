b5f42ac476a3420f2e0a6cfd68c6b0e50020c1ed
MaxHumpherys <maxhumpherys@gmail.com>
2025-01-12 21:19:09 -0700

    update test names

--- server/src/test/java/com/kanban/maxh1231/DemoApplicationTests.java
+++ server/src/test/java/com/kanban/maxh1231/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.kanban.maxh1231;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class KanbanServerTests {
 
     @Test
     void contextLoads() {
